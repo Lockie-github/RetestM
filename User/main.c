6b3442a1edda93ff3f17b0292b3d23d8738b1387
lockie <k303278@126.com>
2022-08-09 10:48:55 +0800

    yac

---
+++ User/main.c
@@ -0,0 +1,120 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  fire
+  * @version V1.0
+  * @date    2013-xx-xx
+  * @brief   ����led
+  ******************************************************************************
+  * @attention
+  *
+  * ʵ��ƽ̨:Ұ�� F103 STM32 ������ 
+  * ��̳    :http://www.firebbs.cn
+  * �Ա�    :https://fire-stm32.taobao.com
+  *
+  ******************************************************************************
+  */ 
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stm32f1xx.h"
+#include "./usart/bsp_debug_usart.h"
+#include "./sdio/bsp_sdio_sdcard.h"
+#include "./sdio/sdio_test.h"
+#include "./led/bsp_led.h"
+#include ".\key\bsp_key.h" 
+//uint32_t test[5]={1,2,3,4,5};
+/**
+  * @brief  ������
+  * @param  ��
+  * @retval ��
+  */
+int main(void)
+{
+  HAL_Init();        
+  /* ����ϵͳʱ��Ϊ72 MHz */ 
+  SystemClock_Config();
+  /*��ʼ��USART ����ģʽΪ 115200 8-N-1���жϽ���*/
+  DEBUG_USART_Config();
+  
+	 /* ��ʼ��LED�� */
+  LED_GPIO_Config();
+	LED_BLUE;	
+	/* ��ʼ���������� */
+	Key_GPIO_Config();
+  
+  /*��ʼ��USART1*/
+  DEBUG_USART_Config();
+  
+	printf("\r\n555\r\n");	
+	
+	printf("555555555555\r\n");			
+	printf("555555555555\r\n");		
+	printf("5555555555555\r\n");		
+	printf("\r\n 55555555555555555\r\n");		
+	
+	printf("\r\n 555555555555555555555555555555....\r\n");	
+  
+  /* Infinite loop */
+ /* while (1)
+  {	
+
+		if(	Key_Scan(KEY1_GPIO_PORT,KEY1_PIN) == KEY_ON)
+		{*/
+   
+			printf("\r\nstart\r\n");	
+		  SD_Test();			
+		//}
+	//} 
+}
+
+
+/**
+  * @brief  System Clock Configuration
+  *         The system Clock is configured as follow : 
+  *            System Clock source            = PLL (HSE)
+  *            SYSCLK(Hz)                     = 72000000
+  *            HCLK(Hz)                       = 72000000
+  *            AHB Prescaler                  = 1
+  *            APB1 Prescaler                 = 2
+  *            APB2 Prescaler                 = 1
+  *            HSE Frequency(Hz)              = 8000000
+  *            HSE PREDIV1                    = 1
+  *            PLLMUL                         = 9
+  *            Flash Latency(WS)              = 2
+  * @param  None
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef clkinitstruct = {0};
+  RCC_OscInitTypeDef oscinitstruct = {0};
+  
+  /* Enable HSE Oscillator and activate PLL with HSE as source */
+  oscinitstruct.OscillatorType  = RCC_OSCILLATORTYPE_HSE;
+  oscinitstruct.HSEState        = RCC_HSE_ON;
+  oscinitstruct.HSEPredivValue  = RCC_HSE_PREDIV_DIV2;
+  oscinitstruct.PLL.PLLState    = RCC_PLL_ON;
+  oscinitstruct.PLL.PLLSource   = RCC_PLLSOURCE_HSE;
+  oscinitstruct.PLL.PLLMUL      = RCC_PLL_MUL9;
+  if (HAL_RCC_OscConfig(&oscinitstruct)!= HAL_OK)
+  {
+    /* Initialization Error */
+    while(1); 
+  }
+
+  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 
+     clocks dividers */
+  clkinitstruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  clkinitstruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  clkinitstruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  clkinitstruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  clkinitstruct.APB1CLKDivider = RCC_HCLK_DIV2;  
+  if (HAL_RCC_ClockConfig(&clkinitstruct, FLASH_LATENCY_0)!= HAL_OK)
+  {
+    /* Initialization Error */
+    while(1); 
+  }
+}
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
